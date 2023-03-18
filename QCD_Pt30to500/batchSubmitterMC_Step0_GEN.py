17adf1adb663da5fbe89af898ef88e79d6c32285
Elena Vernazza <e.vernazza1@campus.unimib.it>
2023-03-18 08:25:16 +0100

    resubmission option

--- QCD_Pt30to500/batchSubmitterMC_Step0_GEN.py
+++ QCD_Pt30to500/batchSubmitterMC_Step0_GEN.py
@@ -1,4 +1,4 @@
-import os
+import os, glob
 from datetime import datetime
 from optparse import OptionParser
 
@@ -6,12 +6,13 @@ from optparse import OptionParser
 # ---------- Step 0 ----------
 
 '''
-python3 batchSubmitterMC_Step0_GEN.py --out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV/GEN \
---maxEvents 2000 --nJobs 500 --queue long --globalTag 124X_mcRun3_2022_realistic_postEE_v1
-'''
-'''
-python3 batchSubmitterMC_Step0_GEN.py --out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV/GEN \
---maxEvents 180 --nJobs 8000 --queue short --globalTag 124X_mcRun3_2022_realistic_postEE_v1
+python3 batchSubmitterMC_Step0_GEN.py --out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X/GEN \
+--maxEvents 180 --nJobs 6000 --queue short --globalTag 124X_mcRun3_2022_realistic_postEE_v1
+python3 batchSubmitterMC_Step0_GEN.py --out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X/GEN \
+--maxEvents 180 --nJobs 6000 --queue short --globalTag 124X_mcRun3_2022_realistic_postEE_v1 --resubmit
+
+python3 batchSubmitterMC_Step0_GEN.py --out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_6000_6500/GEN \
+--maxEvents 500 --nJobs 150 --queue long --globalTag 124X_mcRun3_2022_realistic_postEE_v1 --start_from 6000
 '''
 
 if __name__ == "__main__" :
@@ -22,13 +23,19 @@ if __name__ == "__main__" :
     parser.add_option("--nJobs",     dest="nJobs",     type=int,            default=1,                               help="Number of jobs")
     parser.add_option("--queue",     dest="queue",     type=str,            default='short',                         help="long or short queue")
     parser.add_option("--globalTag", dest="globalTag", type=str,            default='124X_mcRun3_2022_realistic_postEE_v1',   help="Which globalTag to use")
+    parser.add_option("--start_from",dest="start_from",type=int,            default=0)
     parser.add_option("--no_exec",   dest="no_exec",   action='store_true', default=False)
+    parser.add_option("--resubmit",  dest="resubmit",  action='store_true', default=False)
     (options, args) = parser.parse_args()
 
     os.system('mkdir -p '+options.out)
 
-    for idx in range(options.nJobs):
-    # for idx in range(159, 500):
+    resubmitting = 0
+
+    starting_index = int(options.start_from)
+    ending_index = starting_index + int(options.nJobs)
+    print(starting_index, ending_index)
+    for idx in range(starting_index, ending_index):
 
         outJobName  = options.out + '/job_' + str(idx) + '.sh'
         outLogName  = options.out + '/log_' + str(idx) + '.txt'
@@ -38,7 +45,22 @@ if __name__ == "__main__" :
         # it's obtained by summing current Y+M+D+H+M+S+job_number
         # now = datetime.now()
         # randseed = int(now.year) + int(now.month) + int(now.day) + int(now.hour) + int(now.minute) + int(now.second) + idx
-        randseed = idx # to be reproducible
+        randseed = idx+1 # to be reproducible
+
+        if options.resubmit:
+            ListErrJobName = glob.glob(options.out + '/job_' + str(idx) + '.sh.e*')
+            ListOutJobName = glob.glob(options.out + '/job_' + str(idx) + '.sh.o*')
+            if len(ListErrJobName) > 0:
+                if len(os.popen('grep "No such file or directory" '+ListErrJobName[-1]).read()) > 0:
+                    print('resubmitting')
+                    if not options.no_exec:
+                        os.system('rm '+ListErrJobName[-1])
+                        os.system('rm '+ListOutJobName[-1])
+                    resubmitting = resubmitting + 1
+                else:
+                    continue
+            else:
+                continue
 
         cmsRun = "cmsRun MC_Step0_GEN_QCD_Pt30to500_cfg.py outputFile=file:"+outRootName
         cmsRun = cmsRun+" maxEvents="+str(options.maxEvents)+" randseed="+str(randseed)+" globalTag="+options.globalTag
